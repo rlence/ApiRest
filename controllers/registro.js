579322741106a6d70213e1a359bad37fe1033faa
Ricardo Lence <lencericardo@gmail.com>
2019-03-29 11:33:13 +0100

    regitro successful

--- controllers/registro.js
+++ controllers/registro.js
@@ -9,13 +9,13 @@ module.exports = (req , res) => {
        const dataString = fs.readFileSync('./Data/data.json');
        const data = JSON.parse(dataString);
 
-       const {userName, email, password} = req.body;
+       const {userName, email, password} = req.body;//obteniendo datos del request
 
        if(!(userName && email && password)){//comprobando que ingreso todos los campos del formulario
               res.status(400).json({message: "Porfavor ingrese todos los campos del formulario"});
        }
 
-       if(data.password.length < 8){//comprobando longitud del password mayor o igual a 8 caracteres
+       if(password.length < 8){//comprobando longitud del password mayor o igual a 8 caracteres
               res.status(400).json({message: "El password tiene que ser de 8 o mas caracteres"});
        }
 
@@ -23,13 +23,11 @@ module.exports = (req , res) => {
               res.status(400).json({message: "Ya existe un usuario con este nombre"});
        }
        
-       if( data.email.find(user => user.email === email)){//comprobando si exite el email
+       if( data.users.find(user => user.email === email)){//comprobando si exite el email
               res.status(400).json({message: "Ya exite este email con otro usuario"});
        }
 
-
-       const newUser = {
-              id:id(),
+       const newUser = {// objeto para crear nuevo usuario              
               userName,
               email,
               password
@@ -42,7 +40,7 @@ module.exports = (req , res) => {
        const outData = JSON.stringify( data );
 
        //sobrescribiendo la base de datos
-       fs.writeFileSync('../Data/data.json')
+       fs.writeFileSync('./Data/data.json', outData)
 
        //responiendo exitosamente la creacion del nuevo usuario
        res.status(201).json({message: "Usuario creado satisfactoriamente"});
